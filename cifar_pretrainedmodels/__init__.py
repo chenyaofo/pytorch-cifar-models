76a935141bb4d0c276adcb5862953c2637e1fe08
chenyaofo <chenyaofo@gmail.com>
2019-05-06 19:28:41 +0800

    add setup file.

--- cifar_pretrainedmodels/__init__.py
+++ cifar_pretrainedmodels/__init__.py
@@ -2,3 +2,6 @@ from .resnet import cifar_resnet20
 from .resnet import cifar_resnet32
 from .resnet import cifar_resnet44
 from .resnet import cifar_resnet56
+
+
+__version__ = "0.0.1-alpha"
---
+++ setup.py
@@ -0,0 +1,43 @@
+import io
+import os
+import re
+from setuptools import setup, find_packages
+
+
+def read(*names, **kwargs):
+    with io.open(
+            os.path.join(os.path.dirname(__file__), *names),
+            encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+VERSION = find_version('cifar_pretrainedmodels', '__init__.py')
+
+
+def find_requirements(file_path):
+    with open(file_path) as f:
+        return f.read().splitlines()
+
+
+requirements = find_requirements("requirements.txt")
+
+setup(
+    name="cifar_pretrainedmodels",
+    version=VERSION,
+    description="Pretrained models for CIFAR10/100 in PyTorch",
+    url="https://github.com/chenyaofo/CIFAR-pretrained-models",
+    author="chenyaofo",
+    author_email="chenyaofo@gmail.com",
+    packages=find_packages(exclude=['test']),
+    install_requires=requirements,
+)
